59a072951e2bc2020ba9c50b7c90fa1f593e5355
labile <labile.zhu@gmail.com>
2023-07-09 21:55:09 +0800

    fix dev-istio/dev-istio.md

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -38,7 +38,8 @@ html_theme_options = {
     # "path_to_docs": "docs",
     "use_repository_button": True,
     "use_edit_page_button": False,
-    "show_navbar_depth": 8,
+    "show_navbar_depth": 3,
+    "show_toc_level": 3,
     "logo_only": True,
 }
 
